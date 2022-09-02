18cf81ba7432577e8544fac509f55aa186541f0f
Artyom Ivanov <artem-ivanov-1999@mail.ru>
2022-09-02 11:59:24 +0300

    Сделал отображение полученных данных в таблице.

--- MyTraineeTask/MyTraineeTask/ViewController.swift
+++ MyTraineeTask/MyTraineeTask/ViewController.swift
@@ -10,20 +10,35 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     
+    //получает даннае с модели
+    var company: Company? {
+        didSet{
+            
+            // что бы выполнение задачи происходило последовательно в главном потоке
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.textLable.text = self.company?.company.name
+            }
+        }
+    }
+    
     
     //MARK: - TableViewFunc
     
     //отображение количества строк
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return company?.company.employees.count ?? 0
     }
     
     //отображение данных в ячейках
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var cell = UITableViewCell(style: .subtitle, reuseIdentifier: "myCell")
-        cell.textLabel?.text = "hello"
         
+        var cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        var employer = company?.company.employees[indexPath.row]
+        cell.textLabel?.text = "Name: \((employer?.name)!) (phone: \((employer?.phone_number)!))"
+        cell.detailTextLabel?.text = "Skill: \((employer?.skills.description)!)"
         return cell
     }
     
@@ -38,10 +53,18 @@ class ViewController: UIViewController, UITableViewDataSource {
         return tableView
     }()
     
+    lazy var textLable: UILabel = {
+       let lable = UILabel()
+       lable.translatesAutoresizingMaskIntoConstraints = false
+        
+       return lable
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         //MARK: - AddJSON
         
         guard let url = URL(string:"https://run.mocky.io/v3/0c5d58be-3cf8-41c1-9cce-0c5ee681b107")
@@ -57,7 +80,7 @@ class ViewController: UIViewController, UITableViewDataSource {
             
                 do {
                     let company = try JSONDecoder().decode(Company.self, from: data)
-                    print(company.company.employees)
+                    self.company = company
                 }
                 catch {
                     print(error)
@@ -83,6 +106,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         tableView.dataSource = self
         
         view.addSubview(tableView)
+        view.addSubview(textLable)
         
         SetupConstrain()
     }
@@ -90,6 +114,13 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     private func SetupConstrain() {
     
+        //textLable constraints
+        textLable.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        //textLable.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30).isActive
+        textLable.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100).isActive = true
+        
+        
+        // tebleView constraints
         tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
