37ca1235e13c213c37ba2914bb55decadefc5f1c
Artyom Ivanov <artem-ivanov-1999@mail.ru>
2022-09-02 08:23:44 +0300

    Удален storyboard. Добавил tableView на View. Но пока что только пустые троки

--- MyTraineeTask/MyTraineeTask/ViewController.swift
+++ MyTraineeTask/MyTraineeTask/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    //MARK: - TableViewFunc
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+
+    //MARK: - UIElements
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+        ConfigureView()
+        SetupSubviews()
+    }
+    
+    
+    //MARK: - AddElements
+    
+    private func ConfigureView(){
+        
+        view.backgroundColor = .white
+    }
+    
+    private func SetupSubviews() {
+        
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        
+        SetupConstrain()
+    }
+    
+    
+    private func SetupConstrain() {
+    
+        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
-
 }
+
 
